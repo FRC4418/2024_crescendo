d8ae80b67ad374efac8b6418e1482af374fbde30
LukeG65536 <116130542+LukeG65536@users.noreply.github.com>
2024-01-18 18:21:52 -0700

    got angles working

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -51,12 +51,12 @@ public class SwerveSubsystem extends SubsystemBase {
     //  In this case the wheel diameter is 4 inches, which must be converted to meters to get meters/second.
     //  The gear ratio is 6.75 motor revolutions per wheel rotation.
     //  The encoder resolution per motor revolution is 1 per motor revolution.
-    System.out.println(angleConversionFactor + "------------------------------____");
     double driveConversionFactor = SwerveMath.calculateMetersPerRotation(Units.inchesToMeters(3), 6.75, 1);
+    System.out.println(driveConversionFactor + "------------------------------____");
 
     try
     {
-      swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, angleConversionFactor, driveConversionFactor);
+      swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
       // Alternative method if you don't want to supply the conversion factor via JSON files.
       // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, angleConversionFactor, driveConversionFactor);
     } catch (Exception e)
